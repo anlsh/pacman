72f0549d3f0c72dae5aafef29cd0534a165ae2f5
pipsqueaker <anish.moorthy@gmail.com>
2015-06-11 19:51:50 -0400

    Added pausing and player add functionality

--- driver.py
+++ driver.py
@@ -8,11 +8,17 @@ from common import *
 
 class Driver(pyglet.window.Window):
 
-    def __init__(self, width, length):
+    def __init__(self, width, length, number_players=1):
+
+        self.number_players = number_players
+
         super().__init__(width, length)
         self.w = width
         self.l = length
-        self.game = Game("classic.map", xoff=0, yoff=100)
+
+        self.key_states = pyglet.window.key.KeyStateHandler()
+
+        self.game = Game("classic.map", wanted_players=self.number_players, xoff=0, yoff=100)
 
         self.pre_add = True
         self.pre_pause = True
@@ -24,24 +30,26 @@ class Driver(pyglet.window.Window):
 
     def update(self, dt):
 
+        self.push_handlers(self.key_states)
+
+        if self.key_states[pyglet.window.key.K] and not self.pre_add:
+            if self.number_players < self.game.governor.map_max_players
+                self.game = Game("classic.map", wanted_players=self.number_players, xoff=0, yoff=100)
+
+        if self.key_states[pyglet.window.key.O] and not self.pre_pause:
+            self.game.should_update = not self.game.should_update
+
         if not self.game.over:
             for x in self.game.players:
                 self.push_handlers(x.keys)
 
-            if x.keys[pyglet.window.key.P] and not self.pre_add:
-                if len(self.game.players) < self.game.governor.map_max_players:
-                    self.game = Game("classic.map", xoff=0, yoff=100)
-
-            if x.keys[pyglet.window.key.O] and not self.pre_pause:
-                self.game.should_update = not self.game.should_update
-
             self.game.update()
 
         else:
-            self.game = Game("classic.map", xoff=0, yoff=100)
+            self.game = Game("classic.map", wanted_players=self.number_players, xoff=0, yoff=100)
 
-        self.pre_pause = self.game.players[0].keys[pyglet.window.key.O]
-        self.pre_add = self.game.players[0].keys[pyglet.window.key.P]
+        self.pre_add = self.key_states[pyglet.window.key.K]
+        self.pre_pause = self.key_states[pyglet.window.key.O]
 
     def on_draw(self):
         self.clear()
